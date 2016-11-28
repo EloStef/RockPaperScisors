45ada77e4f54844ae240dcebd7cbed91a9c514e2
Stef <dwrona94@gmail.com>
2016-11-28 06:38:17 +0100

    SelectBaseMap

--- www/js/map.js
+++ www/js/map.js
@@ -5,6 +5,41 @@ var blackosmUrl = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
         attribution: blackosmAttrib
     });
 
+var baselayers = {
+    "Map": L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: ''
+    }),
+    "Earth": L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '',
+    maxZoom: 18,
+    })
+}
+
+var overlays = {};
+/*var OpenStreetMap_DE = L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+var HERE_hybridDay = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
+    attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+    subdomains: '1234',
+    mapID: 'newest',
+    app_id: '<your app_id>',
+    app_code: '<your app_code>',
+    base: 'aerial',
+    maxZoom: 18,
+    type: 'maptile',
+    language: 'eng',
+    format: 'png8',
+    size: '256'
+});
+
+var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});*/
+
 function getLastCoords() {
     var lastcords = localStorage.getItem("lastcoords");
     if (lastcords == null)
@@ -19,8 +54,12 @@ function setLastCoords(lat, lng) {
 var map = L.map('map', {
         rotate: true
     })
-    .setView([getLastCoords()[0], getLastCoords()[1]], getLastCoords()[2])
-    .addLayer(blackosm);
+    .setView([getLastCoords()[0], getLastCoords()[1]], getLastCoords()[2]);
+
+map.attributionControl.setPrefix("")
+L.control.layers(baselayers, overlays).addTo(map);
+
+baselayers["Map"].addTo(map);
 
 var MapSystem = function() {
     this.map = map;
@@ -64,6 +103,9 @@ MapSystem.prototype = {
     clearMapLayers: function() {
         this.clearMarkers();
         this.clearLines();
+    },
+    switchBaseMap: function() {
+
     }
 }
 
